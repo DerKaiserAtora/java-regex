92894fbcb1c8dc4553e5b68739ab6988396e2aeb
DerKaiserAtora <suzakuflamepheonix@gmail.com>
2019-08-03 19:35:25 +0700

    complete Main

--- src/atr/regex/Main.java
+++ src/atr/regex/Main.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        String line = "This order was placed for QT3000! OK?";
+        String line = "This order was placed for QT3000! 085795990256 021777888999 OK?";
         String pattern = "(\\d+)";
 
         // Create a Pattern object
@@ -21,8 +21,13 @@ public class Main {
         m = r.matcher(line);
         printer(m);
 
-        String pattern3 = "(order)[was]";
+        String pattern3 = "(order)";
         r = Pattern.compile(pattern3);
+        m = r.matcher(line);
+        printer(m);
+
+        String pattern4 = "(^085)";
+        r = Pattern.compile(pattern4);
         m = r.matcher(line);
         printer(m);
     }
